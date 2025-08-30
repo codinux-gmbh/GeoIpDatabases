5c9841e53fb9bd52e1ec5101a537d785d5f5329c
dankito <git@dankito.net>
2025-08-30 21:22:11 +0200

    Added Continent enum

---
+++ geoip-database-api/src/main/kotlin/net/codinux/geoip/database/Continent.kt
@@ -0,0 +1,17 @@
+package net.codinux.geoip.database
+
+enum class Continent(val code: String) {
+    Africa("AF"),
+
+    Antarctica("AN"),
+
+    Asia("AS"),
+
+    Europe("EU"),
+
+    Oceania("OC"),
+
+    NorthAmerica("NA"),
+
+    SouthAmerica("SA"),
+}

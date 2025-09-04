b95b754e88b7140cfa2dd3c7ae3db07e369294a6
dankito <git@dankito.net>
2025-09-04 23:03:59 +0200

    Listing which database files could not be downloaded

--- GeoIpAPI/src/main/kotlin/net/codinux/geoip/health/DatabaseFilesDownloadedReadinessHealthCheck.kt
+++ GeoIpAPI/src/main/kotlin/net/codinux/geoip/health/DatabaseFilesDownloadedReadinessHealthCheck.kt
@@ -4,11 +4,13 @@ import jakarta.enterprise.event.Observes
 import jakarta.inject.Singleton
 import net.codinux.geoip.config.GeoIpConfig
 import net.codinux.geoip.event.DatabaseFileUpdateAttemptEvent
+import net.codinux.log.collection.ConcurrentSet
 import org.eclipse.microprofile.health.HealthCheck
 import org.eclipse.microprofile.health.HealthCheckResponse
 import org.eclipse.microprofile.health.Readiness
 import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.io.path.absolutePathString
 import kotlin.io.path.exists
 import kotlin.io.path.fileSize
 import kotlin.io.path.isRegularFile
@@ -21,34 +23,48 @@ class DatabaseFilesDownloadedReadinessHealthCheck(
 
     private val allDatabaseFilesAvailable = AtomicBoolean(false)
 
+    private val notDownloadedRequestedDatabaseFiles = ConcurrentSet<Path>()
+
 
     override fun call(): HealthCheckResponse =
         if (allDatabaseFilesAvailable.get()) {
             HealthCheckResponse.up("All requested GeoIP databases have been downloaded successfully.")
         } else {
             HealthCheckResponse.down("""
-                Not all requested GeoIP databases have been downloaded.
-                Please check the logs for more details.
+The following requested GeoIP databases could not be downloaded:
+${notDownloadedRequestedDatabaseFiles.joinToString(separator = "\n") { " - " + it.absolutePathString() }}
+Please check the logs for more details.
             """.trimIndent())
         }
 
 
     fun onDatabaseUpdateAttempt(@Observes event: DatabaseFileUpdateAttemptEvent) {
-        allDatabaseFilesAvailable.set(
-            checkIsNullOrDownloaded(config.ipLocate.asnPath)
-            && checkIsNullOrDownloaded(config.ipLocate.countryPath)
+        notDownloadedRequestedDatabaseFiles.clear()
 
-            && checkIsNullOrDownloaded(config.geoLite2.asnPath)
-            && checkIsNullOrDownloaded(config.geoLite2.countryPath)
-            && checkIsNullOrDownloaded(config.geoLite2.cityPath)
+        val ipLocateAsnCheck = checkIsNullOrDownloaded(config.ipLocate.asnPath, notDownloadedRequestedDatabaseFiles)
+        val ipLocateCountryCheck = checkIsNullOrDownloaded(config.ipLocate.countryPath, notDownloadedRequestedDatabaseFiles)
+
+        val geoLite2AsnCheck = checkIsNullOrDownloaded(config.geoLite2.asnPath, notDownloadedRequestedDatabaseFiles)
+        val geoLite2CountryCheck = checkIsNullOrDownloaded(config.geoLite2.countryPath, notDownloadedRequestedDatabaseFiles)
+        val geoLite2CityCheck = checkIsNullOrDownloaded(config.geoLite2.cityPath, notDownloadedRequestedDatabaseFiles)
+
+        allDatabaseFilesAvailable.set(
+            ipLocateAsnCheck && ipLocateCountryCheck &&
+            geoLite2AsnCheck && geoLite2CountryCheck && geoLite2CityCheck
         )
     }
 
-    private fun checkIsNullOrDownloaded(path: Path?): Boolean =
+    private fun checkIsNullOrDownloaded(path: Path?, notDownloadedFiles: ConcurrentSet<Path>): Boolean =
         if (path == null) { // if database file is not configured it's ok. User doesn't want it, so we don't need to download it
             true
         } else { // otherwise check if file has been downloaded
-            path.exists() && path.isRegularFile() && path.fileSize() > 9_000_000 // all GeoIP databases are at least 9 MB large
+            val isDownloaded = path.exists() && path.isRegularFile() && path.fileSize() > 9_000_000 // all GeoIP databases are at least 9 MB large
+
+            if (isDownloaded == false) {
+                notDownloadedFiles.add(path)
+            }
+
+            isDownloaded
         }
 
 }

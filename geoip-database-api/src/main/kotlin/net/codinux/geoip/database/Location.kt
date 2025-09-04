561f867350607bfacf89192b13747188c2983073
dankito <git@dankito.net>
2025-09-04 08:29:11 +0200

    Renamed accuracyRadius to accuracyRadiusKilometers and added documentation for it

--- geoip-database-api/src/main/kotlin/net/codinux/geoip/database/Location.kt
+++ geoip-database-api/src/main/kotlin/net/codinux/geoip/database/Location.kt
@@ -3,7 +3,14 @@ package net.codinux.geoip.database
 data class Location(
     val latitude: Double,
     val longitude: Double,
-    val accuracyRadius: Int,
+
+    /**
+     * The approximate accuracy radius, in kilometers, around the latitude and longitude for the geographical entity
+     * (country, subdivision, city or postal code) associated with the IP address. We have a 67% confidence that the
+     * location of the end-user falls within the area defined by the accuracy radius and the latitude and longitude
+     * coordinates.
+     */
+    val accuracyRadiusKilometers: Int,
 
     val timeZone: String? = null,
 ) {

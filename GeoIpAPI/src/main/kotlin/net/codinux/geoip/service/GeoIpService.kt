1e305ec7c201dfba773a29fbe60e6284871295cf
dankito <git@dankito.net>
2025-09-05 02:38:40 +0200

    Atomically updating database readers

--- GeoIpAPI/src/main/kotlin/net/codinux/geoip/service/GeoIpService.kt
+++ GeoIpAPI/src/main/kotlin/net/codinux/geoip/service/GeoIpService.kt
@@ -13,30 +13,31 @@ import net.codinux.geoip.database.geolite2.GeoLite2LocalMaxMindGeoIpDatabase
 import net.codinux.geoip.database.iplocate.IPLocateLocalMaxMindGeoIpDatabase
 import net.codinux.geoip.event.ProviderDatabasesDownloadResultEvent
 import net.codinux.log.logger
+import java.util.concurrent.atomic.AtomicReference
 
 @Singleton
 class GeoIpService(
     private val config: GeoIpConfig
 ) {
-    private var geoLite2Database = GeoLite2LocalMaxMindGeoIpDatabase(config.geoLite2.countryPath,
-        config.geoLite2.asnPath, config.geoLite2.cityPath)
+    private var geoLite2Database = AtomicReference(GeoLite2LocalMaxMindGeoIpDatabase(config.geoLite2.countryPath,
+        config.geoLite2.asnPath, config.geoLite2.cityPath))
 
-    private var ipLocateDatabase = IPLocateLocalMaxMindGeoIpDatabase(config.ipLocate.countryPath,
-        config.ipLocate.asnPath)
+    private var ipLocateDatabase = AtomicReference(IPLocateLocalMaxMindGeoIpDatabase(config.ipLocate.countryPath,
+        config.ipLocate.asnPath))
 
     private val log by logger()
 
 
     fun lookupCountry(ipAddress: String): Country? =
-        geoLite2Database.lookupCountry(ipAddress)
-            ?: ipLocateDatabase.lookupCountry(ipAddress)
+        geoLite2Database.get().lookupCountry(ipAddress)
+            ?: ipLocateDatabase.get().lookupCountry(ipAddress)
 
     fun lookupCity(ipAddress: String): City? =
-        geoLite2Database.lookupCity(ipAddress)
+        geoLite2Database.get().lookupCity(ipAddress)
 
     fun lookupAsn(ipAddress: String): AutonomousSystem? =
-        ipLocateDatabase.lookupAsn(ipAddress)
-            ?: geoLite2Database.lookupAsn(ipAddress)
+        ipLocateDatabase.get().lookupAsn(ipAddress)
+            ?: geoLite2Database.get().lookupAsn(ipAddress)
 
 
     fun <T> withCallerIp(request: HttpServerRequest, action: (callerIp: String) -> T) =
@@ -60,15 +61,15 @@ class GeoIpService(
 
     private fun updateDatabaseReaders(event: ProviderDatabasesDownloadResultEvent) {
         when (event.provider) {
-            DatabaseProvider.GeoLite2 -> geoLite2Database = GeoLite2LocalMaxMindGeoIpDatabase(
+            DatabaseProvider.GeoLite2 -> geoLite2Database.set(GeoLite2LocalMaxMindGeoIpDatabase(
                 config.geoLite2.countryPath,
                 config.geoLite2.asnPath, config.geoLite2.cityPath
-            )
+            ))
 
-            DatabaseProvider.IPLocate -> ipLocateDatabase = IPLocateLocalMaxMindGeoIpDatabase(
+            DatabaseProvider.IPLocate -> ipLocateDatabase.set(IPLocateLocalMaxMindGeoIpDatabase(
                 config.ipLocate.countryPath,
                 config.ipLocate.asnPath
-            )
+            ))
         }
     }
 

3cf6f4c15db209c545bbed5fc7bdf9b1b5c72b01
dankito <git@dankito.net>
2025-09-11 12:42:34 +0200

    Renamed docker image from geo-ip to geoip

--- GeoIpAPI/create-jvm-docker-image.sh
+++ GeoIpAPI/create-jvm-docker-image.sh
@@ -1,6 +1,6 @@
 
 ../gradlew build -x test &&
 
-docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/geo-ip-jvm . &&
+docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/geoip-jvm . &&
 
-docker push docker.dankito.net/dankito/geo-ip-jvm
+docker push docker.dankito.net/dankito/geoip-jvm
--- GeoIpAPI/create-native-docker-image.sh
+++ GeoIpAPI/create-native-docker-image.sh
@@ -2,8 +2,8 @@ date &&
 
 ../gradlew build -Dquarkus.native.enabled=true -Dquarkus.package.jar.enabled=false -Dquarkus.native.container-build=true -x test &&
 
-docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/geo-ip . &&
+docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/geoip . &&
 
-docker push docker.dankito.net/dankito/geo-ip &&
+docker push docker.dankito.net/dankito/geoip &&
 
 date

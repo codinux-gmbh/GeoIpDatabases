4914ac8213e1e23105f5e0fdd3256624ef196dc1
dankito <git@dankito.net>
2025-09-11 12:41:42 +0200

    Added start and end date time

--- GeoIpAPI/create-native-docker-image.sh
+++ GeoIpAPI/create-native-docker-image.sh
@@ -1,6 +1,9 @@
+date &&
 
 ../gradlew build -Dquarkus.native.enabled=true -Dquarkus.package.jar.enabled=false -Dquarkus.native.container-build=true -x test &&
 
 docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/geo-ip . &&
 
-docker push docker.dankito.net/dankito/geo-ip
+docker push docker.dankito.net/dankito/geo-ip &&
+
+date
